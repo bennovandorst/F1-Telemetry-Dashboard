058a0551af515f292408520e85465ba41b91d0d4
bennovandorst <9024828@student.zadkine.nl>
2025-05-18 20:44:32 +0200

    Local ip test

--- src/wss.js
+++ src/wss.js
@@ -43,7 +43,7 @@ export const SimRigWebSocketProvider = ({ children }) => {
     };
 
     const createSocket = (simrigId) => {
-        const socketUrl = `ws://localhost:8080/simrig/${simrigId}`;
+        const socketUrl = `ws://192.168.178.199:8080/simrig/${simrigId}`;
         const socket = new WebSocket(socketUrl);
         socketRef.current = socket;
         window.ws = socket;
